e27b340b23356f7512ae84f599e5d1b2c1ff0550
David Lopez <cristiandml3722@gmail.com>
2025-10-07 17:16:35 -0500

    Finished Module-10

--- Module-10/src/shell.ts
+++ Module-10/src/shell.ts
@@ -1,3 +1,7 @@
+import type { Character } from "./model";
+import { createCardGrid } from "./ui";
+import { recallAllCharacters, recallFilteredCharacters } from "./motor";
+
 // Función de ayuda para obtener y validar elementos del DOM de forma segura.
 const getValidatedElement = <T extends HTMLElement>(
   id: string,
@@ -13,9 +17,45 @@ const getValidatedElement = <T extends HTMLElement>(
 document.addEventListener("DOMContentLoaded", () => {
   try {
     // Call elements at the start
-    const characterGrid = getValidatedElement(
+    const characterSection = getValidatedElement(
       "character-container",
       HTMLElement
     );
-  } catch (error) {}
+    const searchInput = getValidatedElement("search-input", HTMLInputElement);
+    const searchButton = getValidatedElement(
+      "search-button",
+      HTMLButtonElement
+    );
+    //
+    const paintCards = (characters: Character[]) => {
+      characterSection.innerHTML = "";
+      createCardGrid(characterSection, characters);
+    };
+
+    const retrieveValue = (): string => {
+      const searchValue = searchInput.value.trim();
+      return searchValue;
+    };
+
+    const searchAndPaint = async () => {
+      const searchValue = retrieveValue();
+      let characters: Character[];
+      if (searchValue === "") {
+        characters = await recallAllCharacters();
+      } else {
+        characters = await recallFilteredCharacters(searchValue);
+      }
+      paintCards(characters);
+    };
+
+    searchButton.addEventListener("click", searchAndPaint);
+    searchInput.addEventListener("keyup", (event: KeyboardEvent) => {
+      if (event.key === "Enter") {
+        searchAndPaint();
+      }
+    });
+    searchAndPaint();
+  } catch (error) {
+    console.error("There was a problem while initializing", error);
+  }
 });
