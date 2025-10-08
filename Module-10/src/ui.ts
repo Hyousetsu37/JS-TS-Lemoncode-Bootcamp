ef9413042d4f2f2ab05530e5eb8882331beaea5f
David Lopez <cristiandml3722@gmail.com>
2025-10-08 11:01:17 -0500

    fixed createCard function to append infoContainer to the end of the card.

--- Module-10/src/ui.ts
+++ Module-10/src/ui.ts
@@ -29,34 +29,46 @@ const createInfoLine = (parent: HTMLElement, tagName: string, info: string) => {
 };
 
 export const createCard = (parent: HTMLElement, character: Character) => {
+  //Create the card as an article
   const createdCard = document.createElement("article");
+  //Add the class to the created Card
   createdCard.classList = "card";
   createdCard.dataset.chName = character.nombre;
+  //Create the Img element
   const chImage = createImg(
     createdCard,
     `http://localhost:3000/${character.imagen}`,
     `Image of the character ${character.nombre}`
   );
+  //Create the title of the card to add the name of the character
   const chName = document.createElement("h3");
+  //Set the inner text of the title to the name of the character
   chName.innerText = character.nombre;
+  //Append both the Title and the image to the card
   createdCard.appendChild(chImage);
   createdCard.appendChild(chName);
-  parent.appendChild(createdCard);
-
+  // Create the container for habilities and speciality
   const infoContainer = createDiv(createdCard, "info-container");
+  //Create the speciality line with the information
   const specialityLine = createInfoLine(
     infoContainer,
     "Especialidad: ",
     `${character.especialidad}.`
   );
+  //Create the hability line with the information
   const habilityLine = createInfoLine(
     infoContainer,
     "Habilidades: ",
     `${character.habilidades.join(", ")}.`
   );
+  //Append the speciality and hability lines to the Info container
   infoContainer.appendChild(specialityLine);
   infoContainer.appendChild(habilityLine);
-
+  //Append the info container to the Card so the information is displayed at the end of the card
+  createdCard.appendChild(infoContainer);
+  //Append the card to the parent to show the card
+  parent.appendChild(createdCard);
+  //Return the card for possible management if needed.
   return createdCard;
 };
 
