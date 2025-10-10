295f98544f675448079fb247294ad22900ac9400
David Lopez <cristiandml3722@gmail.com>
2025-10-10 17:03:41 -0500

    finished module 11

--- Module-11/src/apartadoB/ui.ts
+++ Module-11/src/apartadoB/ui.ts
@@ -4,9 +4,24 @@ const appendTextLine = (parent: HTMLElement, text: string) => {
   parent.appendChild(line);
 };
 
+const createImg = (parent: HTMLElement, src: string) => {
+  const img = document.createElement("img");
+  img.src = src;
+  parent.appendChild(img);
+};
+
+const createCard = (parent: HTMLElement, className: string) => {
+  const card = document.createElement("div");
+  card.className = className;
+  parent.appendChild(card);
+  return card;
+};
+
 export const displayUrls = (parent: HTMLElement, urls: Array<string>) => {
   parent.innerHTML = "";
   urls.forEach((url) => {
-    appendTextLine(parent, url);
+    const currentCard = createCard(parent, "card");
+    createImg(currentCard, url);
+    appendTextLine(currentCard, url);
   });
 };
